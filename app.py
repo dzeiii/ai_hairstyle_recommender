5d5994e2c54f8eb8396ff5e8354d96ae8f2cf7d1
jaaaam <jhamagcasi@gmail.com>
2026-05-24 15:13:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -90,8 +90,7 @@ source_option = st.radio("Choose Input Method:", ("Use Live Camera", "Upload Ima
 
 captured_image = None
 if source_option == "Use Live Camera":
-    camera_input = st.camera_input("Position your face clearly in the center box boundary", 
-                                "💡 For accurate AI analysis: Sit facing a bright light source and ensure your background is plain.")
+    camera_input = st.camera_input("Position your face clearly in the center box boundary","💡 For accurate AI analysis: Sit facing a bright light source and ensure your background is plain.")
     if camera_input is not None:
         captured_image = Image.open(camera_input)
 else:
